6b1c8cbb8444912c31f602917729b1d0cbcf94c1
Newton Ni <nwtnni@gmail.com>
2019-07-26 18:33:56 -0400

    Fetch channel and server ID from environment variables

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,47 @@ use std::env;
 use std::error;
 use std::io;
 
+use std::io::Write;
+use std::str::FromStr;
+
 use std::io::BufRead;
+
+trait Tap: Sized {
+    fn tap<T, F: FnOnce(Self) -> T>(self, f: F) -> T {
+        f(self) 
+    }
+}
+
+impl<T: Sized> Tap for T {}
 
 fn main() -> Result<(), Box<dyn error::Error>> {
 
-    let token = env::var("TOKEN")?;
-    let discord = discord::Discord::from_bot_token(&token)?;
+    let discord = env::var("TOKEN")?
+        .as_str()
+        .tap(discord::Discord::from_bot_token)?;
+
+    let server = env::var("SERVER")?
+        .as_str()
+        .tap(u64::from_str)?
+        .tap(discord::model::ServerId);
+
+    let channel = env::var("CHANNEL")?
+        .as_str()
+        .tap(u64::from_str)?
+        .tap(discord::model::ChannelId);
+
     let stdin = io::stdin();
+    let stdout = io::stdout();
+    let mut out = stdout.lock();
 
     let info = regex::Regex::new(r".*\[Server thread/INFO\]: (.*)")?;
 
     for line in stdin.lock().lines().map(Result::unwrap) {
 
-        println!("{}", line);
+        writeln!(out, "{}", line)?;
 
         if let Some(cap) = info.captures(&line) {
-            println!("{}", &cap[0]);
+            writeln!(out, "{}", &cap[0])?;
         }
     }
 
